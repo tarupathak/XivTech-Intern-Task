9ba0413879382e403361c15089678e8780b0eb5e
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-13 12:29:14 +0530

    applied routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FirstPage from "./First Page/FirstPage";
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/First Page/FirstPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FirstPage = () => {
+  return (
+    <div>First Page</div>
+  )
+}
+
+export default FirstPage
